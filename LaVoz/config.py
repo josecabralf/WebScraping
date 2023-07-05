f3d1e9c8893f61a8af393d1d45e12c71fd40f74a
José Cabral <cabraljose1210@gmail.com>
2023-07-05 08:30:48 -0300

    Arrancado proyecto Voz del Interior Clasificados

--- LaVoz/config.py
+++ LaVoz/config.py
@@ -1,1 +1,2 @@
-URL = 'https://clasificados.lavoz.com.ar/inmuebles/casas?ciudad=cordoba&operacion=venta'
+URL = 'https://clasificados.lavoz.com.ar/inmuebles/casas?ciudad=cordoba&operacion=venta&page=1'
+# Nos vamos a estar fijando solo en la primera pagina para probar
