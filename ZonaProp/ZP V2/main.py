18ed58a199767ffed8c73484809969492d4aedd8
José Cabral <cabraljose1210@gmail.com>
2023-07-21 12:42:14 -0300

    Creados archivos para mayor modularidad y cohesion

--- ZonaProp/ZP V2/main.py
+++ ZonaProp/ZP V2/main.py
@@ -3,24 +3,7 @@ from soup import getSoup
 from math import trunc
 from hilos import *
 from archivos import formarArchivo, asignarValNro
-
-
-def formarLink(i, URL):
-    """Forma links de listados de publicaciones según el criterio de ZonaProp
-
-    Args:
-        i (int): indica el numero de pagina actual
-        URL: url base a editar
-
-    Returns:
-        string: url modificada para acceder a pagina i
-    """
-    if i == 1:
-        return URL
-    link = URL.split('.')
-    link[-2] = link[-2] + f"-pagina-{i}"
-    link = '.'.join(link)
-    return link
+from links import formarLink
 
 
 def main():
