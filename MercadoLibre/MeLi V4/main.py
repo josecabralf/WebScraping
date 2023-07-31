bd262d4036e171b179236401e62fa49fbe0e41ba
José Cabral <cabraljose1210@gmail.com>
2023-07-31 12:01:54 -0300

    MeLi V4: mismos cambios que LaVoz v4 adaptados a MeLi

---
+++ MercadoLibre/MeLi V4/main.py
@@ -0,0 +1,24 @@
+from modules.scrapers.scraperMeLi import scrapPaginaMeLi
+from MeLiConfig import archivos_Meli
+from modules.format.archivos import *
+
+
+def main():
+    archivo = abrirArchivo()
+
+    nro = asignarValNro(archivos_Meli)
+    for line in archivo.readlines():
+        try:
+            link = line.replace('\n', '')
+            print(f'Scrapeando Link {nro}: {link}')
+            scrapPaginaMeLi(link, nro)
+            nro += 1
+            agregarALeidos(line, True)
+        except:
+            print(f'No se pudo scrapear Link {nro}')
+            agregarALeidos(line, False)
+    archivo.close()
+
+
+if __name__ == "__main__":
+    main()
