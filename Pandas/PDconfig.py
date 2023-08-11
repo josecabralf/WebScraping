c74f87b0313e586824e4d0dc65a293e5e4e15e3e
José Cabral <cabraljose1210@gmail.com>
2023-08-11 09:33:46 -0300

    Estructura de Carpetas Pandas/Dataframes/ZonaP

--- Pandas/PDconfig.py
+++ Pandas/PDconfig.py
@@ -1,7 +1,14 @@
 path_LV = '../LaVoz/LaVoz V5/Casas/'
 path_ML = '../MercadoLibre/MeLi V4/Casas/'
-path_ZP = '../ZonaProp/ZP V5/Casas/'
 
 LaVoz = './DataFrames/LaVoz/data_LV.csv'
 MeLi = './DataFrames/MeLi/data_ML.csv'
+
+# ZONAPROP
+path_ZP = '../ZonaProp/ZP V5/Casas/'
 ZonaP = './DataFrames/ZonaP/data_ZP.csv'
+path_ZP_inmuebles = './DataFrames/ZonaP/Analisis/Inmuebles/'
+path_ZP_inmuebles_img = './DataFrames/ZonaP/Analisis/Inmuebles/img/'
+path_ZP_inmuebles_MD = './DataFrames/ZonaP/Analisis/Inmuebles/Manantiales-Docta/'
+path_ZP_terrenos = './DataFrames/ZonaP/Analisis/Terrenos/'
+path_ZP_terrenos_img = './DataFrames/ZonaP/Analisis/Terrenos/img/'
