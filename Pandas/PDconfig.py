13a393b95a4d1801f4f5b4d1ed1a58ec8824ab90
José Cabral <cabraljose1210@gmail.com>
2023-08-11 12:07:59 -0300

    Movidos proyectos finalizados de Scrap a Scrapers

--- Pandas/PDconfig.py
+++ Pandas/PDconfig.py
@@ -1,11 +1,11 @@
-path_LV = '../LaVoz/LV V5/Casas/'
-path_ML = '../MercadoLibre/ML V4/Casas/'
+path_LV = '../Scrapers/LV V5/Casas/'
+path_ML = '../Scrapers/ML V4/Casas/'
 
 LaVoz = './DataFrames/LaVoz/data_LV.csv'
 MeLi = './DataFrames/MeLi/data_ML.csv'
 
 # ZONAPROP
-path_ZP = '../ZonaProp/ZP V5/Casas/'
+path_ZP = '../Scrapers/ZP V5/Casas/'
 ZonaP = './DataFrames/ZonaP/data_ZP.csv'
 path_ZP_inmuebles = './DataFrames/ZonaP/Analisis/Inmuebles/'
 path_ZP_inmuebles_img = './DataFrames/ZonaP/Analisis/Inmuebles/img/'
