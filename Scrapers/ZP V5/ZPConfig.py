8bb29e48c13e740a36788ce37d651e53d4ff133d
José Cabral <cabraljose1210@gmail.com>
2023-08-15 10:20:14 -0300

    Cambios minimos

--- Scrapers/ZP V5/ZPConfig.py
+++ Scrapers/ZP V5/ZPConfig.py
@@ -4,9 +4,9 @@ URL_Base = 'https://www.zonaprop.com.ar'
 archivos_filtros = './Links/filtros.txt'
 publicados_recientes = './Links/ultima_semana.txt'
 archivos_ZonaProp = './Casas/'
-revisionesFecha = './Revisiones/revisionesFecha.txt'
+revisionesFecha = './Links/revisionesFecha.txt'
 comprimidos = './Comprimidos/'
 
-path_driver = "./driver/chromedriver"
+path_driver = "../driver/chromedriver"
 
 cols = 'id;tipoPropiedad;precioUSD;fechaUltimaActualizacion;tipoVendedor;terrenoTotal;terrenoEdificado;cantDormitorios;cantBanos;cantCochera;barrio;ciudad;coordX;coordY;activo;URL\n'
